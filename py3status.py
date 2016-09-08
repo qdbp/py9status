48d5c7a461c2d211eee9fdbd3c8b45723f488dd3
shmo <evgeny.a.naumov@gmail.com>
2016-09-08 12:25:45 -0400

    nvidia gpu support; basic working version of click handling

--- py3status.py
+++ py3status.py
@@ -1,13 +1,15 @@
 #! /usr/bin/python
 
+from collections import deque
 import concurrent.futures as cfu
 from datetime import datetime as dtt
 import heapq as hpq
 import json
 from re import findall
 from subprocess import check_output
-from sys import stdout
+from sys import stdout, stdin
 import time
+from threading import Lock
 import traceback as trc
 
 # base 16 tomorrow colors
@@ -31,14 +33,13 @@ BASE0E = '#B294BB'
 BASE0F = '#A3685A'
 
 
-def chunk_to_json(name, chunk, padding, **kwargs):
+def chunk_to_json(unit, chunk, padding,
+                  def_pango=True, def_border=True,
+                  def_separator=False, **kwargs):
     '''
     generates a string snippet corresponding to one i3bar element.
 
-    a single logical unit can generate more than one such element, hence
-    separators should be handled by the status_command and not i3bar itself.
-
-    all kwargs are according to the i3 bar api specification
+    all kwargs are according to the i3 bar api specification.
     '''
 
     # chunks can return None to signify no output
@@ -49,16 +50,29 @@ def chunk_to_json(name, chunk, padding, **kwargs):
     if isinstance(chunk, str):
         chunk = {'full_text': chunk}
 
-    # use pango by default
-    chunk.update({'markup': 'pango'})
+    # change some defaults:
+    # - use pango
+    if def_pango:
+        chunk.update({'markup': 'pango'})
+    # - add a nice border
+    if def_border:
+        chunk.update({'border': BASE02})
+    # - turn off the separator
+    if not def_separator:
+        chunk.update({'separator': 'false',
+                      'separator_block_width': 0})
 
     # if the chunk provides no name, use the unit's name
     if 'name' not in chunk:
-        chunk.update({'name': name})
+        chunk.update({'name': unit.name})
 
     chunk['full_text'] = padding*' ' + chunk['full_text'] + padding*' '
-
+    
+    # apply any kwarg overrides
     chunk.update(kwargs)
+    # apply any unit-set overrides
+    chunk.update(unit.overrides)
+    unit.overrides = {}
 
     return json.dumps(chunk)
 
@@ -103,81 +117,140 @@ class PY3Status:
             minimum number of seconds to sleep between unit poll sweeps
         '''
         self.units = units
-        self._q = []
+        self.units_by_name = {u.name: u for u in units}
+
+        self._unit_q = []
+        self._click_q = deque()
         self._exe = cfu.ThreadPoolExecutor(max_workers=4)
 
-        self.chunk_lines = {u.name: u.get_chunk()
+        self.unit_outputs = {u.name: u.get_chunk()
                             for u in self.units}
         self.chunk_padding = chunk_padding
 
         self.min_sleep = min_sleep
 
         for u in self.units:
-            hpq.heappush(self._q, (time.time() + u.ival, u))
+            hpq.heappush(self._unit_q, (time.time() + u.ival, u))
 
-    def add_unit(self, unit, where=None):
-        if where is not None:
-            self.units.insert(where, unit)
-        else:
-            self.units.append(unit)
-
-    def del_unit(self, unit):
-        ix = self.units.index(unit)
-        del self.units[ix]
-        del self.chunk_lines[unit.name]
-
-    def get_output(self):
+    def write_statusline(self):
         o = []
         for u in self.units:
-            chunk_json = chunk_to_json(u.name, self.chunk_lines[u.name],
+            chunk_json = chunk_to_json(u, self.unit_outputs[u.name],
                                        self.chunk_padding)
             if chunk_json:
                 o.append(chunk_json)
 
-        return ','.join(o)
+        stdout.write('[' + ','.join(o) + '],\n')
+        stdout.flush()
+    
+    def _read_clicks(self):
+        # TODO: maybe find a proper json parser, not this DIY hackery
 
-    def _exe_unit(self, unit):
+        while True:
+            # "burn" the opening [\n or ,\n
+            stdin.read(2)
+            try:
+                # TODO: ...
+                # maybe this would be more reasonable in cython
+                # but I don't want to pull in third-party json streamers
+                s = ''
+                while True:
+                    c = stdin.read(1)
+                    s += c
+                    if c == '}':
+                        break
+
+                click = json.loads(s)
+                self._exe.submit(self._exe_unit,
+                                 self.units_by_name[click.pop('name')],
+                                 clicked=True, click=click)
+
+            except Exception:
+                continue
+
+    def _exe_unit(self, unit, clicked=False, click=None):
         try:
+            if clicked:
+                assert click is not None
+                unit.handle_click(click)
             o = unit.get_chunk()
-            self.chunk_lines[unit.name] = o
+            self.unit_outputs[unit.name] = o
+            # assume statusline is costly enough to process such that
+            # having it rewritten on every unit execution would be costly
+            # hence, we aggregate in unit_outputs, then print in a batch
+            # unless the unit has been clicked and needs an immediate update
+            if clicked:
+                self.write_statusline()
         except Exception:
             trc.print_exc()
-            self.chunk_lines[unit.name] = None
+            self.unit_outputs[unit.name] =\
+                colorify('unit "{}" failed'.format(unit.name), '#FF0000')
 
     def run(self):
         # header
         stdout.write('{"version":1,"click_events":true}\n[\n')
         stdout.flush()
 
+        # start input reader
+        self._exe.submit(self._read_clicks)
+
         while True:
             now = time.time()
-            while self._q[0][0] < now:
-                t, u = self._q[0]
+            while self._unit_q[0][0] < now:
+                t, u = self._unit_q[0]
                 # threads - don't GIL on me
                 self._exe.submit(self._exe_unit, u)
-                hpq.heapreplace(self._q, (now + u.ival, u))
+                hpq.heapreplace(self._unit_q, (now + u.ival, u))
 
-            o = '[' + self.get_output() + '],\n'
-            stdout.write(o)
-            stdout.flush()
+            # writing a statuline is -assumed- somehwat costly on the i3
+            # end, therefore we don't just roll it into exe_unit, unless
+            # the unit is clicked
+            self.write_statusline()
 
-            time.sleep(max(self.min_sleep, self._q[0][0] - time.time()))
+            time.sleep(max(self.min_sleep, self._unit_q[0][0] - time.time()))
 
 
 class PY3Unit:
     def __init__(self, name=None, ival=1.):
+        '''
+        self.overrides:
+            chunk_to_json will, after each invocation of get_chunk,
+            augment the returned json with these parameters, and clear this
+            dict.
+        '''
+        # self.ovr_lock:
+        #     `self.overrides` is accessed in a potentially non-thread-safe
+        #     manner from both the `py3s.write_statusline` and the `py3u.handle_click`
+        #     threads. consequently, we acquire the unit's `ovr_lock` before
+        #     making changes.
+        # '''
         if name is None:
             name = self.__class__.__name__
         self.name = name
         self.ival = ival
+        self.overrides = {}
+        # TODO: I think the GIL will prevent dict.updates from different
+        # threads from exploding, but I'm not sure
+        # self.ovr_lock = Lock()
 
     def get_chunk(self):
         '''
-        return a dict suitable for input to chunk_to_json.
+        get the unit's output to display on the line. returns str or dict.
 
-        allows pango markup.
+        the return value should either be a string, which will be assumed to
+        be the full_text value of the unit's output, and which permits pango
+        markup; or a dict, assumed to conform to the i3bar api and which will
+        be serialized as given (pango markup will still be enabled).
         '''
         return 'unimplemented unit output'
+
+    def handle_click(self, click):
+        '''
+        handle the i3-generated `click`, passed as a dictionary. returns None.
+
+        see i3 documentation and example code for click's members
+        '''
+        self.overrides.update({'border': BASE08})
 
     # comparison functions for heapq not to crash when times are equal
     def __lt__(self, other):
@@ -301,35 +374,36 @@ class PY3Net(PY3Unit):
     def __init__(self, i_f, down_ival=30, smooth=1/5, **kwargs):
         super().__init__(**kwargs)
         self.i_f = i_f
-        # TODO: we don't ever care if these are closed properly...
-        self.rx_file = open('/sys/class/net/{}/statistics/rx_bytes'
-                            .format(i_f), 'r')
-        self.tx_file = open('/sys/class/net/{}/statistics/tx_bytes'
-                            .format(i_f), 'r')
-        self.operfile = open('/sys/class/net/{}/operstate'
-                             .format(i_f), 'r')
+
+        self.rx_file = '/sys/class/net/{}/statistics/rx_bytes'.format(i_f)
+        self.tx_file = '/sys/class/net/{}/statistics/tx_bytes'.format(i_f)
+        self.operfile = '/sys/class/net/{}/operstate'.format(i_f)
         self.mark = None
         self.down_ival = down_ival
         self.smooth = smooth
 
     def _get_rx_tx(self):
-        rx = int(self.rx_file.read())
-        tx = int(self.tx_file.read())
-        self.rx_file.seek(0)
-        self.tx_file.seek(0)
+        with open(self.rx_file, 'r') as f:
+            rx = int(f.read())
+        with open(self.tx_file, 'r') as f:
+            tx = int(f.read())
         return rx, tx
 
     def get_chunk(self):
-        if "down" in self.operfile.read():
-            self.operfile.seek(0)
-            self.ival = self.down_ival
-            return None
+        prefix = 'net {} '.format(self.i_f)
+        try:
+            with open(self.operfile, 'r') as f:
+                if "down" in f.read():
+                    self.ival = self.down_ival
+                    return prefix + colorify('down', BASE08)
+        except FileNotFoundError:
+            return prefix + colorify('down', BASE08)
 
         if self.mark is None:
             self.mark = time.time()
             self.old_rx, self.old_tx = self._get_rx_tx()
             self.old_rxr, self.old_txr = 0, 0
-            return None
+            return prefix + colorify('loading', BASE0E)
         else:
             rx, tx = self._get_rx_tx()
 
@@ -346,23 +420,24 @@ class PY3Net(PY3Unit):
             sfs = ['B/s', 'B/s']
             vals = [rxr, txr]
             for ix in range(2):
-                for mag, sf in [(30, 'GiB/s'), (20, 'MiB/s'), (10, 'KiB/s')]:
+                for mag, sf in [(30, 'G/s'), (20, 'M/s'), (10, 'K/s')]:
                     if vals[ix] > 1 << mag:
                         vals[ix] /= 1 << mag
                         sfs[ix] = sf
                         break
 
-        return ('net {} '.format(self.i_f) +
-                '[up {:6.1f} {:>5s}] '.format(vals[1], sfs[1]) +
-                '[down {:6.1f} {:>5s}] '.format(vals[0], sfs[0]))
+        return (prefix +
+                '[u {:6.1f} {:>3s}] '.format(vals[1], sfs[1]) +
+                '[d {:6.1f} {:>3s}] '.format(vals[0], sfs[0]))
 
 
 def main():
     units = [PY3NVGPU(ival=5.),
              PY3Mem(ival=3.),
              PY3CPU(),
-             PY3Net('vpn-ca', name='net_e0', ival=0.5),
+             PY3Net('vpn-ca', name='net_vpn'),
              PY3Time(ival=0.7)]
+
     py3s = PY3Status(units)
     py3s.run()
 
